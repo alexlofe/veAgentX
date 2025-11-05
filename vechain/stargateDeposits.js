6432b18fc4b1e252ab0384c4528be713cb9bc1fd
alexlofe <alejandro.lofe@gmail.com>
2025-11-05 14:36:26 +0000

    fix: Deposits and Withdrawals

--- vechain/stargateDeposits.js
+++ vechain/stargateDeposits.js
@@ -5,10 +5,16 @@ export async function getLastTransfers(address, network = process.env.VECHAIN_NE
 
   const deposits = await thor.logs.filterTransferLogs({
     criteriaSet: [{ sender: address, recipient: '0x1856c533ac2d94340aaa8544d35a5c1d4a21dee7'}],
-    range: { unit: "block", from: 20_000_000,},
-    options: { limit: 3 },
+    range: { unit: "block", from: 20_000_000 },
+    options: { limit: 1 },
     order: 'desc',
   });
 
-  return deposits;
+  const d = deposits[0];
+
+  return {
+    from: address,
+    to: d.recipient,
+    value: (BigInt(d.amount) / 1000000000000000000n).toString(),
+  };
 }
--- vechain/stargateWithdrawals.js
+++ vechain/stargateWithdrawals.js
@@ -5,10 +5,16 @@ export async function getLastTransfers(address, network = process.env.VECHAIN_NE
 
   const withdrawals = await thor.logs.filterTransferLogs({
     criteriaSet: [{ sender: '0x1856c533ac2d94340aaa8544d35a5c1d4a21dee7', recipient: address}],
-    range: { unit: "block", from: 20_000_000,},
-    options: { limit: 3 },
+    range: { unit: "block", from: 20_000_000 },
+    options: { limit: 1 },
     order: 'desc',
   });
 
-  return withdrawals;
+  const w = withdrawals[0];
+
+  return {
+    from: address,
+    to: w.recipient,
+    value: (BigInt(w.amount) / 1000000000000000000n).toString(),
+  };
 }
